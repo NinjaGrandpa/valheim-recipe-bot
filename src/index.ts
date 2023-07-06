b4d7a9cda547640ba01d50dbf9adc56ff42b4a9a
NinjaGrandpa <max@jenslov.se>
2023-07-06 14:40:15 +0200

    Created connection to MongoDb

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { Client } from "discord.js";
 import { IntentOptions } from "./config/IntentOptions";
+import { connectDatabase } from "./database/connectDatabase";
 
 (async () => {
   const Bot = new Client({ intents: IntentOptions });
 
+  Bot.on("ready", () => console.log("Connected to Discord!"));
+
+  await connectDatabase();
   await Bot.login(process.env.BOT_TOKEN);
 })();
