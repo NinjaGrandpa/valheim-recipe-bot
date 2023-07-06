078caf32b10d45adee695b546527e35c48596e20
NinjaGrandpa <max@jenslov.se>
2023-07-06 14:44:06 +0200

    fixed type coercion of MONGO_URI

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import { Client } from "discord.js";
 import { IntentOptions } from "./config/IntentOptions";
 import { connectDatabase } from "./database/connectDatabase";
+import { validateEnv } from "./utils/validateEnv";
 
 (async () => {
+  if (!validateEnv()) return;
+
   const Bot = new Client({ intents: IntentOptions });
 
   Bot.on("ready", () => console.log("Connected to Discord!"));
 
   await connectDatabase();
+  
   await Bot.login(process.env.BOT_TOKEN);
 })();
