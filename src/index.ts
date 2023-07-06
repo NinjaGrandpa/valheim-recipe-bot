e98fac2ea38b5e0bbe7812bae926529c907c791e
NinjaGrandpa <max@jenslov.se>
2023-07-06 14:05:10 +0200

    Setup Finished

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello, World!");
+import { Client } from "discord.js";
+import { IntentOptions } from "./config/IntentOptions";
+
+(async () => {
+  const Bot = new Client({ intents: IntentOptions });
+
+  await Bot.login(process.env.BOT_TOKEN);
+})();
