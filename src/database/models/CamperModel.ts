5fe85d93e9c25a8a32dc23c301469765566a06c3
NinjaGrandpa <max@jenslov.se>
2023-07-06 15:19:35 +0200

    Finished 'CamperModel.ts'

--- src/database/models/CamperModel.ts
+++ src/database/models/CamperModel.ts
@@ -1,3 +1,11 @@
 import { Document, model, Schema } from "mongoose";
+import { ICamper } from "../../interfaces/ICamper";
 
+export const Camper = new Schema({
+  discordId: String,
+  round: Number,
+  day: Number,
+  timestamp: Number,
+});
 
+export default model<ICamper>("camper", Camper);
