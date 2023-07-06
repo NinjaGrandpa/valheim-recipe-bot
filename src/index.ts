6b068d0c0ff3786793a540fc69e1c97d92eb783c
NinjaGrandpa <max@jenslov.se>
2023-07-06 14:48:52 +0200

    added event listener for onInteraction event

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Client } from "discord.js";
 import { IntentOptions } from "./config/IntentOptions";
 import { connectDatabase } from "./database/connectDatabase";
 import { validateEnv } from "./utils/validateEnv";
+import { onInteraction } from "./events/onInteraction";
 
 (async () => {
   if (!validateEnv()) return;
@@ -9,8 +10,12 @@ import { validateEnv } from "./utils/validateEnv";
   const Bot = new Client({ intents: IntentOptions });
 
   Bot.on("ready", () => console.log("Connected to Discord!"));
+  Bot.on(
+    "interactionCreate",
+    async (interaction) => await onInteraction(interaction)
+  );
 
   await connectDatabase();
-  
+
   await Bot.login(process.env.BOT_TOKEN);
 })();
