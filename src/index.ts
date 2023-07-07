dd1213f935f41c60cacd1c68721d0e20f092856b
NinjaGrandpa <max@jenslov.se>
2023-07-07 16:01:04 +0200

    Added event 'onReady'

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,14 @@ import { IntentOptions } from "./config/IntentOptions";
 import { connectDatabase } from "./database/connectDatabase";
 import { validateEnv } from "./utils/validateEnv";
 import { onInteraction } from "./events/onInteraction";
+import { onReady } from "./events/onReady";
 
 (async () => {
   if (!validateEnv()) return;
 
   const Bot = new Client({ intents: IntentOptions });
 
-  Bot.on("ready", () => console.log("Connected to Discord!"));
+  Bot.on("ready", async () => onReady(Bot));
   Bot.on(
     "interactionCreate",
     async (interaction) => await onInteraction(interaction)
