e6b5d946cbcd32089f7a337ad88155dc6dd490ce
NinjaGrandpa <max@jenslov.se>
2023-07-14 12:49:24 +0200

    Changed name of database

--- src/database/connectDatabase.ts
+++ src/database/connectDatabase.ts
@@ -1,6 +1,6 @@
 import { connect } from "mongoose";
 
 export const connectDatabase = async () => {
-  await connect(process.env.MONGO_URI as string);
+  await connect(process.env.MONGO_URI as string, {dbName: "valheim-recipe-bot"});
   console.log("Database Connected!");
 };
