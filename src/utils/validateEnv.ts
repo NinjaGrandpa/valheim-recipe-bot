3576ac46fcec067d16d709378cdd2b0214bf0983
NinjaGrandpa <max@jenslov.se>
2023-07-19 14:44:15 +0200

    Added eslint to the project

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -1,15 +1,15 @@
 // Validates enviorment variables
 
 export const validateEnv = () => {
-    if (!process.env.BOT_TOKEN) {
-        console.warn("Missing Discord bot token.");
-        return false;        
-    }
+  if (!process.env.BOT_TOKEN) {
+    console.warn('Missing Discord bot token.');
+    return false;
+  }
 
-    if (!process.env.MONGO_URI) {
-        console.warn("Missing MongoDb connection string.");
-        return false;
-    }
+  if (!process.env.MONGO_URI) {
+    console.warn('Missing MongoDb connection string.');
+    return false;
+  }
 
-    return true;
+  return true;
 };
